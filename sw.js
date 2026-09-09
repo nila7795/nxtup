bb4afb5e151fcaf53b2b1a4db42c6c77746447ca
nila7795 <k.amedov@icloud.com>
2026-09-09 10:50:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nxtup-v10';
+const CACHE='nxtup-classic-sunset-v11';
 const CORE=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
